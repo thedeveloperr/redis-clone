9304714981f552baafb3a6e3f451337e28c1f054
Mohit Gupta <mohitgupta3mail@gmail.com>
2020-06-17 14:16:01 +0530

    HTTP server to accept concurrent user requests.

--- main.go
+++ main.go
@@ -1,7 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+var inMemoryDb *InMemoryStore
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.Error(w, "404 not found.", http.StatusNotFound)
+		return
+	}
+	switch r.Method {
+	case "POST":
+		if err := r.ParseForm(); err != nil {
+			fmt.Fprintf(w, "ParseForm() err: %v", err)
+			return
+		}
+		command := r.FormValue("command")
+		result := inMemoryDb.ProcessCommand(command)
+		fmt.Fprintf(w, "%s\n", result)
+
+	default:
+		fmt.Fprintf(w, "Sorry, only POST method supported.")
+	}
+}
 
 func main() {
-	fmt.Println("Redis Clone in progress")
+	inMemoryDb = CreateInMemStore()
+	http.HandleFunc("/", handler)
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
